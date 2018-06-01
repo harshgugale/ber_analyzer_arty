28b82f343ade9a1b03db50e8b11d6dc6eeca2f9f
harshgugale <hashok.gugale.ece14@iitbhu.ac.in>
2018-06-01 18:53:36 +0530

    Updated to calculate bit wise error and global bit error

--- rx_top.py
+++ rx_top.py
@@ -5,25 +5,31 @@ from prbs import *
 class _RX(Module):
 	def __init__(self, data_width):
 		self.rx_prbs_config = rx_prbs_config = Signal(2)
-		self.inp = inp = Signal(data_width)
-		self.error = error = Signal(32)
-		self.prbs_error = Signal(data_width)
+		self.rxdata = rxdata = Signal(data_width)
+		#self.errors = errors = Signal(32)
+		self.bit_wise_errors = Signal(data_width)
 
 		prbs_rx = PRBSRX(data_width)
 		self.submodules += prbs_rx
 
 		self.comb += [
 			prbs_rx.config.eq(rx_prbs_config),
-			prbs_rx.i.eq(inp),
-			self.error.eq(prbs_rx.errors),
-			self.prbs_error.eq(prbs_rx.prbs_errors)
+			prbs_rx.i.eq(rxdata),
+			If(
+			rx_prbs_config == 00, 
+			#self.errors.eq(0) ,
+			self.bit_wise_errors.eq(0)
+			).Else(
+			#self.errors.eq(prbs_rx.errors),
+			self.bit_wise_errors.eq(prbs_rx.bit_wise_errors)
+			)
 		]
 
 class RX(Module):
 	def __init__ (self, data_width):
 		self.rx_prbs_config = Signal(2)
 		self.inp1 = inp1 = Signal(data_width)
-		self.error = CSRStatus(32)
+		self.errors = CSRStatus(32)
 
 		_rx = _RX(data_width)
 		self.submodules+= _rx
@@ -31,5 +37,5 @@ class RX(Module):
 		self.comb += [
 			_rx.rx_prbs_config.eq(self.rx_prbs_config),
 			_rx.inp.eq(inp1),
-			self.error.status.eq(_rx.error)
+			self.errors.status.eq(_rx.errors)
 		]
