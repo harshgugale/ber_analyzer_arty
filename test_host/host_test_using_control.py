0e5c2686fc04f55d0da981eb3e90fcd8ac459de1
harshgugale <hashok.gugale.ece14@iitbhu.ac.in>
2018-06-07 23:46:06 +0530

    Assertions and BER calc according to changes in control_prbs

--- test_host/host_test_using_control.py
+++ test_host/host_test_using_control.py
@@ -1,9 +1,14 @@
+import sys 
+sys.path.append('..')
+
+from litex.soc.tools.remote import RemoteClient
 from control_prbs import *
 
-prcon = PRBSControl()
-prcon.openCon()
+wb = RemoteClient()
+wb.open()
+prcon = PRBSControl(wb.regs,"top")
 prcon.setErrMask(0,20)
 prcon.setPRBSConfig(7,7)
-print(prcon.calcBER(5))
-prcon.setErrMask(0.5,20)
-print(prcon.calcBER(5))
+print(prcon.calcBER(5,20))
+prcon.setErrMask(0.2,20)
+print(prcon.calcBER(5,20))
