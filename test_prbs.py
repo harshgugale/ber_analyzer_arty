c9f938dcc006f333922f366ae4ff2adcbc12b2bd
Florent Kermarrec <florent@enjoy-digital.fr>
2018-06-13 07:33:23 +0200

    test_prbs: add configurable delay for tx to rx loopback

--- test_prbs.py
+++ test_prbs.py
@@ -3,19 +3,23 @@ from migen import *
 from prbs import PRBSTX, PRBSRX
 
 class DUT(Module):
-    def __init__(self, width):
+    def __init__(self, width, delay=16):
         self.submodules.tx = PRBSTX(width)
         self.submodules.rx = PRBSRX(width)
         # connect tx to rx
-        self.comb += [
-            self.rx.i.eq(self.tx.o)
-        ]
+        data = self.tx.o
+        for i in range(delay):
+            new_data = Signal(32)
+            self.sync += new_data.eq(data)
+            data = new_data
+        self.comb += self.rx.i.eq(data)
+
 
 def tb(dut):
     # start tx and wait some cycles to allow rx to synchronize
     yield dut.tx.config.eq(0b01)
     yield dut.rx.config.eq(0b00)
-    for i in range(16):
+    for i in range(128):
         yield
     # start rx and verify we dont have errors:
     yield dut.tx.config.eq(0b01)
